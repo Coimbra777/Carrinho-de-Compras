e561be9da2517ba8165e2c77c2eab08e5b56aea7
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-11-12 21:42:10 -0300

    gerenciamento do estado do input

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,42 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import styled from "styled-components";
 
 const StyledSearchBar = styled.form`
   .search-bar {
     display: flex;
+    width: 25rem;
+    height: 3rem;
     background-color: #fff;
-    width: 100%;
-    max-width: 500px;
     justify-content: space-between;
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+  }
 
     .search-input {
-      /* Adicione os estilos do input aqui */
+      outline:none
       border: none;
       padding: 10px;
       flex: 1;
     }
 
     .search-button {
-      /* Adicione os estilos do botão aqui */
       border: none;
       background-color: #3498db;
       color: #fff;
       padding: 10px;
       cursor: pointer;
     }
-  }
 `;
 
 const SearchBar: React.FC = () => {
+  const [value, setValue] = useState("");
   return (
     <StyledSearchBar>
       <div className="search-bar">
         <input
           type="search"
+          value={value}
           placeholder="Buscar produtos"
           className="search-input"
+          onChange={({ target }) => setValue(target.value)}
           required
         />
         <button type="submit" className="search-button">
